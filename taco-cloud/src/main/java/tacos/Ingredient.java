92f6e0cc5100419ea6228dd5d8363f0747140f98
黎万成 <wb-liwc01@sdc.icbc.com.cn>
2025-07-16 00:30:08 +0800

    2.1.1 构建领域类

--- taco-cloud/src/main/java/tacos/Ingredient.java
+++ taco-cloud/src/main/java/tacos/Ingredient.java
@@ -1,5 +1,15 @@
 package tacos;
 
+import lombok.Data;
+@Data
 public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type Type;
+
+    public enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
 
 }
